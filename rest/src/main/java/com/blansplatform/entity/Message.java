7b7cfd1f8549db9f650d1e996536f8b87a162b1c
shieran <shieran@mail.ru>
2022-03-20 17:09:04 +0300

    imports

--- rest/src/main/java/com/blansplatform/entity/Message.java
+++ rest/src/main/java/com/blansplatform/entity/Message.java
@@ -1,6 +1,12 @@
 package com.blansplatform.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.Objects;
